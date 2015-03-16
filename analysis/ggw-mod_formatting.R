c5fb8afb9fb152b324b9b679e9fd7897f1689fc5
kgweisman <kweisman@stanford.edu>
2015-03-16 11:19:13 -0700

    read in run 02 json objects and create anonymized data file

--- analysis/ggw-mod_formatting.R
+++ analysis/ggw-mod_formatting.R
@@ -13,13 +13,15 @@ rm(list=ls())
 
 # --- READING IN DATA OBJECTS -------------------------------------------------
 
-# set working directory for india
+# ----------> run-01 (2015-03-13) ---------------------------------------------
+
+# set working directory
 setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-01/")
 
 # mike's json for-loop
 files <- dir("production-results/")
 
-d.raw <- data.frame()
+d.raw_01 <- data.frame()
 
 for(i in 1:length(files)) {
   # gather files
@@ -33,7 +35,7 @@ for(i in 1:length(files)) {
   id <- data.frame(
     matrix(
     data = c("subid", "condition", "age", "gender", "ethnicity", "education", 
-               "religionChild", "religionNow", "job", "maritalStatus", 
+               "religionChild", "religionNow", "job", "d.raw_02atus", 
                "children", "country", "englishNative", "politicalIdeology", 
                "studyMoralPhil", "vegetarian", "beliefGod", "beliefAfterlife", 
                "beliefTradition", "beliefRules", "beliefLeader", "comments", 
@@ -41,27 +43,27 @@ for(i in 1:length(files)) {
     nrow = 78, ncol = 0))
 
   # subject-level data: identity
-  id$subid = paste0("S",i)
+  id$subid = paste0("S01",i)
   id$condition = jd$answers$data$newData$condition
   
   # subject-level data: demographics
   id$age = ifelse(jd$answers$data$newData$age == "", "NA", jd$answers$data$newData$age)
   id$gender = ifelse(jd$answers$data$newData$gender == "", "NA", jd$answers$data$newData$gender)
-  id$ethnicity = ifelse(jd$answers$data$newData$ethnicity == "", "NA", jd$answers$data$newData$ethnicity)
+#   id$ethnicity = ifelse(jd$answers$data$newData$ethnicity == "", "NA", jd$answers$data$newData$ethnicity)
   id$education = ifelse(jd$answers$data$newData$education == "", "NA", jd$answers$data$newData$education)
-  id$religionChild = ifelse(jd$answers$data$newData$religionChild == "", "NA", jd$answers$data$newData$religionChild)
+#   id$religionChild = ifelse(jd$answers$data$newData$religionChild == "", "NA", jd$answers$data$newData$religionChild)
 #   id$religionNow = ifelse(jd$answers$data$newData$religionNow == "", "NA", jd$answers$data$newData$religionNow)
   id$job = ifelse(jd$answers$data$newData$job == "", "NA", jd$answers$data$newData$job)
-  id$maritalStatus = ifelse(jd$answers$data$newData$maritalStatus == "", "NA", jd$answers$data$newData$maritalStatus)
+#   id$maritalStatus = ifelse(jd$answers$data$newData$maritalStatus == "", "NA", jd$answers$data$newData$maritalStatus)
   id$children = ifelse(jd$answers$data$newData$children == "", "NA", jd$answers$data$newData$children)
   id$country = ifelse(jd$answers$data$newData$country == "", "NA", jd$answers$data$newData$country)
-  id$englishNative = ifelse(jd$answers$data$newData$englishNative == "", "NA", jd$answers$data$newData$englishNative)
+#   id$englishNative = ifelse(jd$answers$data$newData$englishNative == "", "NA", jd$answers$data$newData$englishNative)
   id$politicalIdeology = ifelse(jd$answers$data$newData$politicalIdeology == "", "NA", jd$answers$data$newData$politicalIdeology)
-  id$studyMoralPhil = ifelse(jd$answers$data$newData$studyMoralPhil == "", "NA", jd$answers$data$newData$studyMoralPhil)
+#   id$studyMoralPhil = ifelse(jd$answers$data$newData$studyMoralPhil == "", "NA", jd$answers$data$newData$studyMoralPhil)
 #   id$vegetarian = ifelse(jd$answers$data$newData$vegetarian == "", "NA", jd$answers$data$newData$vegetarian)
-  id$beliefGod = ifelse(jd$answers$data$newData$beliefGod == "", "NA", jd$answers$data$newData$beliefGod)
+#   id$beliefGod = ifelse(jd$answers$data$newData$beliefGod == "", "NA", jd$answers$data$newData$beliefGod)
   id$beliefAfterlife = ifelse(jd$answers$data$newData$beliefAfterlife == "", "NA", jd$answers$data$newData$beliefAfterlife)
-  id$beliefTradition = ifelse(jd$answers$data$newData$beliefTradition == "", "NA", jd$answers$data$newData$beliefTradition)
+#   id$beliefTradition = ifelse(jd$answers$data$newData$beliefTradition == "", "NA", jd$answers$data$newData$beliefTradition)
 #   id$beliefRules = ifelse(jd$answers$data$newData$beliefRules == "", "NA", jd$answers$data$newData$beliefRules)
   id$beliefLeader = ifelse(jd$answers$data$newData$beliefLeader == "", "NA", jd$answers$data$newData$beliefLeader)
   id$comments = ifelse(jd$answers$data$newData$comments == "", "NA", jd$answers$data$newData$comments)
@@ -74,15 +76,83 @@ for(i in 1:length(files)) {
   id$rt = jd$answers$data$newData$trialData$rt
 
   # bind into same dataframe
-  d.raw <- bind_rows(d.raw, id)
+  d.raw_01 <- bind_rows(d.raw_01, id)
 }
 
-glimpse(d.raw)
+glimpse(d.raw_01)
+
+# ----------> run-02 (2015-03-16) ---------------------------------------------
+
+# set working directory
+setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-02/")
+
+# mike's json for-loop
+files <- dir("production-results/")
+
+d.raw_02 <- data.frame()
+
+for(i in 1:length(files)) {
+  # gather files
+  f = files[i]
+  jf <- paste("production-results/",f,sep="")
+  
+  # parse JSON object
+  jd <- fromJSON(paste(readLines(jf), collapse=""))
+  
+  # store relevant variables in dataframe 
+  id <- data.frame(
+    matrix(
+      data = c("subid", "condition", "age", "gender", "ethnicity", "education", 
+               "religionChild", "religionNow", "job", "maritalStatus", 
+               "children", "country", "englishNative", "politicalIdeology", 
+               "studyMoralPhil", "vegetarian", "beliefGod", "beliefAfterlife", 
+               "beliefTradition", "beliefRules", "beliefLeader", "comments", 
+               "trialNum", "leftCharacter", "rightCharacter", "response", "rt"),
+      nrow = 78, ncol = 0))
+  
+  # subject-level data: identity
+  id$subid = paste0("S02",i)
+  id$condition = jd$answers$data$newData$condition
+  
+  # subject-level data: demographics
+  id$age = ifelse(jd$answers$data$newData$age == "", "NA", jd$answers$data$newData$age)
+  id$gender = ifelse(jd$answers$data$newData$gender == "", "NA", jd$answers$data$newData$gender)
+#   id$ethnicity = ifelse(jd$answers$data$newData$ethnicity == "", "NA", jd$answers$data$newData$ethnicity)
+  id$education = ifelse(jd$answers$data$newData$education == "", "NA", jd$answers$data$newData$education)
+#   id$religionChild = ifelse(jd$answers$data$newData$religionChild == "", "NA", jd$answers$data$newData$religionChild)
+  #   id$religionNow = ifelse(jd$answers$data$newData$religionNow == "", "NA", jd$answers$data$newData$religionNow)
+  id$job = ifelse(jd$answers$data$newData$job == "", "NA", jd$answers$data$newData$job)
+#   id$maritalStatus = ifelse(jd$answers$data$newData$maritalStatus == "", "NA", jd$answers$data$newData$maritalStatus)
+  id$children = ifelse(jd$answers$data$newData$children == "", "NA", jd$answers$data$newData$children)
+  id$country = ifelse(jd$answers$data$newData$country == "", "NA", jd$answers$data$newData$country)
+#   id$englishNative = ifelse(jd$answers$data$newData$englishNative == "", "NA", jd$answers$data$newData$englishNative)
+  id$politicalIdeology = ifelse(jd$answers$data$newData$politicalIdeology == "", "NA", jd$answers$data$newData$politicalIdeology)
+#   id$studyMoralPhil = ifelse(jd$answers$data$newData$studyMoralPhil == "", "NA", jd$answers$data$newData$studyMoralPhil)
+  #   id$vegetarian = ifelse(jd$answers$data$newData$vegetarian == "", "NA", jd$answers$data$newData$vegetarian)
+#   id$beliefGod = ifelse(jd$answers$data$newData$beliefGod == "", "NA", jd$answers$data$newData$beliefGod)
+  id$beliefAfterlife = ifelse(jd$answers$data$newData$beliefAfterlife == "", "NA", jd$answers$data$newData$beliefAfterlife)
+#   id$beliefTradition = ifelse(jd$answers$data$newData$beliefTradition == "", "NA", jd$answers$data$newData$beliefTradition)
+  #   id$beliefRules = ifelse(jd$answers$data$newData$beliefRules == "", "NA", jd$answers$data$newData$beliefRules)
+  id$beliefLeader = ifelse(jd$answers$data$newData$beliefLeader == "", "NA", jd$answers$data$newData$beliefLeader)
+  id$comments = ifelse(jd$answers$data$newData$comments == "", "NA", jd$answers$data$newData$comments)
+  
+  # trial-level data:                    
+  id$trialNum = jd$answers$data$newData$trialData$trialNum
+  id$leftCharacter = jd$answers$data$newData$trialData$leftCharacter
+  id$rightCharacter = jd$answers$data$newData$trialData$rightCharacter
+  id$response = jd$answers$data$newData$trialData$response
+  id$rt = jd$answers$data$newData$trialData$rt
+  
+  # bind into same dataframe
+  d.raw_02 <- bind_rows(d.raw_02, id)
+}
+
+glimpse(d.raw_02)
 
 # --- TIDYING -----------------------------------------------------------------
 
 # clean up variables
-d_tidy = d.raw %>%
+d_tidy = full_join(d.raw_01, d.raw_02) %>%
   mutate(subid = factor(subid),
          condition = ifelse(condition == "Emotion Recognition",
                             "EmotionRecognition",
@@ -90,18 +160,18 @@ d_tidy = d.raw %>%
          condition = factor(condition),
          age = as.numeric(age),
          gender = factor(gender),
-         ethnicity = factor(ethnicity), # redo for multiple selected
+#          ethnicity = factor(ethnicity), # redo for multiple selected
          education = factor(education),
-         religionChild = factor(religionChild), # redo for multiple selected
+#          religionChild = factor(religionChild), # redo for multiple selected
 #          religionNow = factor(religionNow), # redo for multiple selected
          children = factor(children),
-         englishNative = factor(englishNative),
+#          englishNative = factor(englishNative),
          politicalIdeology = factor(politicalIdeology),         
-         studyMoralPhil = factor(studyMoralPhil),         
+#          studyMoralPhil = factor(studyMoralPhil),         
 #          vegetarian = factor(vegetarian),         
-         beliefGod = factor(beliefGod),         
+#          beliefGod = factor(beliefGod),         
          beliefAfterlife = factor(beliefAfterlife),         
-         beliefTradition = factor(beliefTradition),         
+#          beliefTradition = factor(beliefTradition),         
 #          beliefRules = factor(beliefRules),         
          beliefLeader = factor(beliefLeader),         
          leftCharacter = factor(leftCharacter),         
@@ -120,9 +190,9 @@ glimpse(d_tidy)
 # --- WRITING ANONYMIZED CSV --------------------------------------------------
 
 # write to de-identified csv file
-write.csv(d_tidy, "/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/data/run-01_2015-03-13_data_anonymized.csv")
+write.csv(d_tidy, "/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/data/run-01&02_2015-03-16_data_anonymized.csv")
 
-d = read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/data/run-01_2015-03-13_data_anonymized.csv")[-1] # get rid of column of obs numbers
+d = read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/data/run-01&02_2015-03-16_data_anonymized.csv")[-1] # get rid of column of obs numbers
 
 # view comments
 comments = d %>%
