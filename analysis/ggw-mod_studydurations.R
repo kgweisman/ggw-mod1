c765493f52046dc88edd610d82659a178c74508d
kgweisman <kweisman@stanford.edu>
2015-03-16 11:50:38 -0700

    add run 02 to study durations

--- analysis/ggw-mod_studydurations.R
+++ analysis/ggw-mod_studydurations.R
@@ -3,13 +3,17 @@
 # library
 library("chron")
 
+# --- READING IN DATA OBJECTS -------------------------------------------------
+
+# ----------> run-01 (2015-03-13) ---------------------------------------------
+
 # set working directory for india
 setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-01/")
 
 # mike's json for-loop
 files <- dir("production-results/")
 
-d.raw <- data.frame()
+d.raw_01 <- data.frame()
 
 for(i in 1:length(files)) {
   # gather files
@@ -29,12 +33,47 @@ for(i in 1:length(files)) {
   id$SubmitTime = jd$SubmitTime
   
   # bind into same dataframe
-  d.raw <- bind_rows(d.raw, id)
+  d.raw_01 <- bind_rows(d.raw_01, id)
 }
 
-glimpse(d.raw)
+glimpse(d.raw_01)
 
-d_times = d.raw %>%
+# ----------> run-02 (2015-03-13) ---------------------------------------------
+
+# set working directory for india
+setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-02/")
+
+# mike's json for-loop
+files <- dir("production-results/")
+
+d.raw_02 <- data.frame()
+
+for(i in 1:length(files)) {
+  # gather files
+  f = files[i]
+  jf <- paste("production-results/",f,sep="")
+  
+  # parse JSON object
+  jd <- fromJSON(paste(readLines(jf), collapse=""))
+  
+  # store relevant variables in dataframe 
+  id <- data.frame(
+    matrix(
+      data = c("AcceptTime", "SubmitTime"),
+      nrow = 1, ncol = 0))
+  
+  id$AcceptTime = jd$AcceptTime  
+  id$SubmitTime = jd$SubmitTime
+  
+  # bind into same dataframe
+  d.raw_02 <- bind_rows(d.raw_02, id)
+}
+
+glimpse(d.raw_02)
+
+# --- TIDYING -----------------------------------------------------------------
+
+d_times = full_join(d.raw_01, d.raw_02) %>%
   mutate(startTime = chron(times = substr(AcceptTime, 12, 19), format = 'h:m:s'),
          endTime = chron(times = substr(SubmitTime, 12, 19), format = 'h:m:s'),
          duration = endTime - startTime,
@@ -44,3 +83,4 @@ summary(d_times)
 
 qplot(d_times$duration_mins,
       binwidth = 1)
+
