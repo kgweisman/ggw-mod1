77a107e8aca9d4787e4529554f11d4652b8223de
kgweisman <kweisman@stanford.edu>
2017-05-11 21:15:00 -0700

    update durations

--- analysis/ggw-mod_studydurations.R
+++ analysis/ggw-mod_studydurations.R
@@ -7,15 +7,15 @@ library("langcog")
 
 # --- READING IN DATA OBJECTS -------------------------------------------------
 
-# ----------> run-01 (2015-03-13) ---------------------------------------------
+# ----------> pilot-A (2017-05-11) ---------------------------------------------
 
-# set working directory for run 01
-setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-01/")
+# set working directory for pilot A
+setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1b/turk/pilot-A/")
 
 # mike's json for-loop
 files <- dir("production-results/")
 
-d.raw_01 <- data.frame()
+d.raw_A <- data.frame()
 
 for(i in 1:length(files)) {
   # gather files
@@ -35,115 +35,48 @@ for(i in 1:length(files)) {
   id$SubmitTime = jd$SubmitTime
   
   # bind into same dataframe
-  d.raw_01 <- bind_rows(d.raw_01, id)
+  d.raw_A <- bind_rows(d.raw_A, id)
 }
 
-glimpse(d.raw_01)
+glimpse(d.raw_A)
 
-# ----------> run-02 (2015-03-16) ---------------------------------------------
+# ----------> run-01 (2017-05-12) ---------------------------------------------
 
-# set working directory for run 02
-setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-02/")
-
-# mike's json for-loop
-files <- dir("production-results/")
-
-d.raw_02 <- data.frame()
-
-for(i in 1:length(files)) {
-  # gather files
-  f = files[i]
-  jf <- paste("production-results/",f,sep="")
-  
-  # parse JSON object
-  jd <- fromJSON(paste(readLines(jf), collapse=""))
-  
-  # store relevant variables in dataframe 
-  id <- data.frame(
-    matrix(
-      data = c("AcceptTime", "SubmitTime"),
-      nrow = 1, ncol = 0))
-  
-  id$AcceptTime = jd$AcceptTime  
-  id$SubmitTime = jd$SubmitTime
-  
-  # bind into same dataframe
-  d.raw_02 <- bind_rows(d.raw_02, id)
-}
-
-glimpse(d.raw_02)
-
-# ----------> run-03 (2015-03-27) ---------------------------------------------
-
-# set working directory for run 03
-setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-03/")
-
-# mike's json for-loop
-files <- dir("production-results/")
-
-d.raw_03 <- data.frame()
-
-for(i in 1:length(files)) {
-  # gather files
-  f = files[i]
-  jf <- paste("production-results/",f,sep="")
-  
-  # parse JSON object
-  jd <- fromJSON(paste(readLines(jf), collapse=""))
-  
-  # store relevant variables in dataframe 
-  id <- data.frame(
-    matrix(
-      data = c("AcceptTime", "SubmitTime"),
-      nrow = 1, ncol = 0))
-  
-  id$AcceptTime = jd$AcceptTime  
-  id$SubmitTime = jd$SubmitTime
-  
-  # bind into same dataframe
-  d.raw_03 <- bind_rows(d.raw_03, id)
-}
-
-glimpse(d.raw_03)
-
-# ----------> run-04 (2015-03-27) ---------------------------------------------
-
-# set working directory for run 04
-setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1/turk/run-04/")
-
-# mike's json for-loop
-files <- dir("production-results/")
-
-d.raw_04 <- data.frame()
-
-for(i in 1:length(files)) {
-  # gather files
-  f = files[i]
-  jf <- paste("production-results/",f,sep="")
-  
-  # parse JSON object
-  jd <- fromJSON(paste(readLines(jf), collapse=""))
-  
-  # store relevant variables in dataframe 
-  id <- data.frame(
-    matrix(
-      data = c("AcceptTime", "SubmitTime"),
-      nrow = 1, ncol = 0))
-  
-  id$AcceptTime = jd$AcceptTime  
-  id$SubmitTime = jd$SubmitTime
-  
-  # bind into same dataframe
-  d.raw_04 <- bind_rows(d.raw_04, id)
-}
-
-glimpse(d.raw_04)
+# # set working directory for run 01
+# setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-mod/ggw-mod1b/turk/run-01/")
+# 
+# # mike's json for-loop
+# files <- dir("production-results/")
+# 
+# d.raw_01 <- data.frame()
+# 
+# for(i in 1:length(files)) {
+#   # gather files
+#   f = files[i]
+#   jf <- paste("production-results/",f,sep="")
+#   
+#   # parse JSON object
+#   jd <- fromJSON(paste(readLines(jf), collapse=""))
+#   
+#   # store relevant variables in dataframe 
+#   id <- data.frame(
+#     matrix(
+#       data = c("AcceptTime", "SubmitTime"),
+#       nrow = 1, ncol = 0))
+#   
+#   id$AcceptTime = jd$AcceptTime  
+#   id$SubmitTime = jd$SubmitTime
+#   
+#   # bind into same dataframe
+#   d.raw_01 <- bind_rows(d.raw_01, id)
+# }
+# 
+# glimpse(d.raw_01)
 
 # --- TIDYING -----------------------------------------------------------------
 
-d_times = full_join(d.raw_01, d.raw_02) %>%
-  full_join(d.raw_03) %>%
-  full_join(d.raw_04) %>%
+d_times = d.raw_A %>%
+  # full_join(d.raw_01) %>%
   mutate(startTime = chron(times = substr(AcceptTime, 12, 19), format = 'h:m:s'),
          endTime = chron(times = substr(SubmitTime, 12, 19), format = 'h:m:s'),
          duration = endTime - startTime,
@@ -155,4 +88,4 @@ qplot(d_times$duration_mins,
       binwidth = 1)
 
 # bootstrap 95% confidence intervals
-multi_boot.data.frame(data = d_times, column = "duration_mins", statistics_functions = c("ci_lower", "mean", "ci_upper"))
+multi_boot(data = d_times, column = "duration_mins", statistics_functions = c("ci_lower", "mean", "ci_upper"))
